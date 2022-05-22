fbe8fc48988c4a3b051ed00f90d82167117abbe2
melancholiai <samit2310@gmail.com>
2022-05-22 16:23:29 +0300

    refactor: changes according to pr

--- src/change/models/changeManager.ts
+++ src/change/models/changeManager.ts
@@ -10,7 +10,7 @@ export class ChangeManager {
   public constructor(@inject(Services.LOGGER) private readonly logger: Logger) {}
 
   public mergeChanges(changes: ChangeWithMetadata[], changesetId: number): [string, IdMapping[], string[]] {
-    this.logger.info({ msg: 'started changes merging', amount: changes.length });
+    this.logger.info({ msg: 'started changes merging', count: changes.length });
 
     const [change, idsToCreate, idsToDelete] = mergeChanges(changes, changesetId);
     return [changeToXml({ osmChange: change }), idsToCreate, idsToDelete];
