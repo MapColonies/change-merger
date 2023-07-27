56af15b1b9ff9497769e54c3272320ac053f5a08
NatalieShaked <shakednatalie@gmail.com>
2023-07-27 16:38:14 +0300

    fix: server initialization config error

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,14 @@ import 'reflect-metadata';
 import './common/tracing';
 import { createServer } from 'http';
 import { container } from 'tsyringe';
-import { get } from 'config';
+import config from 'config';
 import { Logger } from '@map-colonies/js-logger';
 import { createTerminus } from '@godaddy/terminus';
 import { getApp } from './app';
 import { DEFAULT_SERVER_PORT, SERVICES } from './common/constants';
 import { IServerConfig } from './common/interfaces';
 
-const serverConfig = get<IServerConfig>('server');
+const serverConfig = config.get<IServerConfig>('server');
 const port: number = parseInt(serverConfig.port) || DEFAULT_SERVER_PORT;
 const app = getApp();
 
